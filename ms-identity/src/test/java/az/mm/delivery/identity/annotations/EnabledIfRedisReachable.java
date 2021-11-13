c244afa272cda15ce7f27a0b0af80fc967290ba4
Mushfiq <mushfiqazeri@gmail.com>
2021-11-14 01:47:47 +0400

    Fixed checkstyle issue

--- ms-identity/src/test/java/az/mm/delivery/identity/annotations/EnabledIfRedisReachable.java
+++ ms-identity/src/test/java/az/mm/delivery/identity/annotations/EnabledIfRedisReachable.java
@@ -1,6 +1,5 @@
 package az.mm.delivery.identity.annotations;
 
-
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.lang.annotation.Retention;
@@ -11,7 +10,7 @@ import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-@Target({ METHOD, TYPE, ANNOTATION_TYPE })
+@Target({METHOD, TYPE, ANNOTATION_TYPE})
 @Retention(RUNTIME)
 @ExtendWith(EnabledIfRedisReachableCondition.class)
 public @interface EnabledIfRedisReachable {
